467cfbeacf283beb9b657b157c63520ab1156a53
Hannah Nelson <nexec64@gmail.com>
2026-04-04 23:54:59 +0000

    This is now a working tool

--- setup.py
+++ setup.py
@@ -4,7 +4,14 @@ setup(name="snapshot",
     version="0.1",
     packages=find_packages(),
     zip_safe=False,
+    install_requires=[
+        "prompt_toolkit"
+    ],
     entry_points={
-        "console_scripts": ['snapshot=snapshot.snapshot:snapshot']
+        "console_scripts": [
+            'ss=snapshot.snapshot:snapshot',
+            'sr=snapshot.snapshot:snapshot_rereference',
+            'si=snapshot.snapshot:snapshot_inspect',
+            ]
     }
 )
