ed675019ddc47f933e220a75651e1112ea85df1b
Hannah Nelson <nexec64@gmail.com>
2026-04-04 22:24:05 +0000

    Got a structure of some sort. It doesn't work yet!

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name="snapshot",
+    version="0.1",
+    packages=find_packages(),
+    zip_safe=False,
+    entry_points={
+        "console_scripts": ['snapshot_script_name=snapshot.snapshot:rawFEED']
+    }
+)
